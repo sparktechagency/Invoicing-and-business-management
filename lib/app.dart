91b4593e0ffdd8241f60a4724158baedfa0ee391
mdsakibulhasansanto7 <mdsakibulhasansanto7@gmail.com>
2026-03-14 09:54:52 +0600

    Feat: Initial folder structure create and setup

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:invoicing_business_management/features/onboarding/presentation/pages/splash_screen.dart';
+
+class InvoicingBusinessManagement extends StatelessWidget {
+  const InvoicingBusinessManagement({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SplashScreen(),
+    );
+  }
+}
